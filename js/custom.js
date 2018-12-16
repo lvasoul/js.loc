9235dfc169dc7599a308ecd276db9ec9392c8a7e
WIN-QL9QT1JL0P1\Vladimir <lvasoul1982@gmail.com>
2018-12-16 18:23:35 +0200

    finish

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,32 @@
-function methodOne() {debugger
-    var createEl = document.createElement('div');
+function methodOne() {
     var val = document.querySelector('#methodone').value;
-    return;
+    var errTxt = errCheck(val);
+    if (errTxt !== '') {
+        document.getElementById("result").innerHTML = errTxt;
+    }
+    else {
+        var diameter = val / 3.14;
+        var diameter = diameter.toFixed(1);
+        document.getElementById("result").innerHTML = 'Diametr: ' + diameter;
+    }
+}
+
+
+
+// Check Type Errors
+function errCheck(e){
+    if ( e == '' ){
+       return '... я все вижу, ты ничего не вводил!!';
+     }else if( e <= 0 ){
+       return 'Введите только положительное значение';
+     }else if( isInt(e) == false ){
+       return 'Введите только целое число.';
+     }else{
+        return '';
+     }
+}
+
+function isInt(value) {
+     let reg = /^[0-9]+$/;
+     return ( reg.test(value) ) ? true : false;
 }
