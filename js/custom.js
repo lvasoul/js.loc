bdb4cb5f29956504fc032af521a9ed855af8884e
WIN-QL9QT1JL0P1\Vladimir <lvasoul1982@gmail.com>
2018-12-17 09:02:32 +0200

    ESLint formatting

--- js/custom.js
+++ js/custom.js
@@ -1,32 +1,31 @@
 function methodOne() {
-    var val = document.querySelector('#methodone').value;
-    var errTxt = errCheck(val);
-    if (errTxt !== '') {
-        document.getElementById("result").innerHTML = errTxt;
-    }
-    else {
-        var diameter = val / 3.14;
-        var diameter = diameter.toFixed(1);
-        document.getElementById("result").innerHTML = 'Diametr: ' + diameter;
-    }
+  var val = document.querySelector('#methodone').value;
+  var errTxt = errCheck(val);
+  if (errTxt == '') {
+    var diameter = val / 3.14;
+    diameter = diameter.toFixed(1);
+    document.getElementById('result').innerHTML = 'Diametr: ' + diameter;
+  } else {
+    document.getElementById('result').innerHTML = errTxt;
+  }
 }
 
 
 
 // Check Type Errors
-function errCheck(e){
-    if ( e == '' ){
-       return '... я все вижу, ты ничего не вводил!!';
-     }else if( e <= 0 ){
-       return 'Введите только положительное значение';
-     }else if( isInt(e) == false ){
-       return 'Введите только целое число.';
-     }else{
-        return '';
-     }
+function errCheck(e) {
+  if (e == '') {
+    return '... я все вижу, ты ничего не вводил!!';
+  } else if (e <= 0) {
+    return 'Введите только положительное значение';
+  } else if (isInt(e) == false) {
+    return 'Введите только целое число.';
+  } else {
+    return '';
+  }
 }
 
 function isInt(value) {
-     let reg = /^[0-9]+$/;
-     return ( reg.test(value) ) ? true : false;
+  let reg = /^[0-9]+$/;
+  return (reg.test(value)) ? true : false;
 }
