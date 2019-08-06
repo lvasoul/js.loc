1dbb4425c14727fc9900e70a56c8a151d718bf35
vlavrinets <lvasoul1982@gmail.com>
2019-08-06 20:08:44 +0300

    kama and storm settings

--- js/custom.js
+++ js/custom.js
@@ -1,30 +1,28 @@
-let num = 1705;
+// Century From Year
 
-function century(year) {
-    for (let i = 0; i < year; i++) {
-        if () {
-            
-        }
-    }
-    // if (year <= 100) {
-    //    
-    // } else {
-    // }
-    // switch (year) {
-    //     case year <= 100:
-    //         console.log(1);
-    //         break;
-    //     case year <= 200:
-    //         console.log(1);
-    //         break;
-    // }
-    return year;
+// function century(year) {
+//     var result = 0;
+//     for (let i = 100; i < year; i+=100) {
+//             result = i / 100;
+//     }
+//     return result + 1;
+// }
+//
+// // SMARTY decision
+// function century2(year) {
+//     return Math.ceil(year/100); //using ceiling method to round up to nearest century (100)
+// }
+
+
+// Expressions Matter
+let a, b, c = 0;
+
+function expressionMatter(a, b, c) {
+    a * (b + c) = d
+    a * b * c = d
+    a + b * c = d
+    (a + b) * c = d
+    return // highest achievable result
 }
 
-console.log(century(num));
-
-// Test.assertEquals(century(1705), 18, 'Testing for year 1705');
-// Test.assertEquals(century(1900), 19, 'Testing for year 1900');
-// Test.assertEquals(century(1601), 17, 'Testing for year 1601');
-// Test.assertEquals(century(2000), 20, 'Testing for year 2000');
-// Test.assertEquals(century(89), 1, 'Testing for year 89');
+console.log(expressionMatter(a, b, c));
